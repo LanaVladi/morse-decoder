7e7f492c24c0ee640821344d671f2b981d24f7ec
Svetlana <lanavladi1605gmail.com>
2022-03-09 20:11:10 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,8 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    // const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+let arr = [];
+for (let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.slice(i, i + 10));
+    // console.log(expr); 
+    // console.log(arr);
 }
+const twoZero = '';
+const dotTen = '.';
+const dashEleven = '-';
+const space = ' ';
 
+arr = arr.map((elem) => elem.replace(/00/g, twoZero)
+.replace(/10/g, dotTen)
+.replace(/11/g, dashEleven)
+.replace(/\*\*\*\*\*\*\*\*\*\*/g, space));
+//   console.log(space);
+//   console.log(arr);
+
+arr = arr.map((elem) => elem !== ' ' ? MORSE_TABLE[elem] : ' ');
+let str = arr.join('')
+return str;
+// console.log(str);
+}
 module.exports = {
     decode
 }
